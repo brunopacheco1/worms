31cde36bd30699382ba1c652cea4494e1652f762
Bruno da Silva <brunopacheco1@yahoo.com>
2019-08-08 22:56:36 +0200

    Initializing the repo.

---
+++ src/main/java/com/dev/bruno/worms/services/GreetingService.java
@@ -0,0 +1,12 @@
+package com.dev.bruno.worms.services;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}
