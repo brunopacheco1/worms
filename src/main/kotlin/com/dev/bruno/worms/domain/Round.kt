42df5804b63c135e094005cd3e304b75b7d0297f
Bruno da Silva <brunopacheco1@yahoo.com>
2019-08-22 00:45:23 +0200

    - Reviewing domain and dto classes;

--- src/main/kotlin/com/dev/bruno/worms/domain/Round.kt
+++ src/main/kotlin/com/dev/bruno/worms/domain/Round.kt
@@ -7,10 +7,12 @@ import javax.persistence.OneToMany
 
 @Entity
 data class Round(
-        val status: RoundStatus,
+        val roundOrder: Long,
         @ManyToOne
         val match: Match
 ) : Persistable<Long>() {
+
+    var status = RoundStatus.WAITING_PLAYERS
 
     @OneToMany(mappedBy = "round", cascade = [(CascadeType.ALL)], orphanRemoval = true)
     val players: Set<PlayerRound> = hashSetOf()
