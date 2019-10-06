f69011dd90570b3f0ddcdab8d27ca3b7bfa3184d
Bruno da Silva <brunopacheco1@yahoo.com>
2019-10-06 23:01:14 +0200

    Starting integration with backend

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { NewPlayer } from "../model/new-player.model";
+import { Player } from "../model/player.model";
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  private static LOGIN_FIELD = "USER_LOGIN";
+  private static PLAYER_FIELD = "LOGGED_USER";
 
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  getNickname(): string {
-    return localStorage.getItem(AuthService.LOGIN_FIELD);
-  }
-
-  login(nickname: string) {
-    localStorage.setItem(AuthService.LOGIN_FIELD, nickname);
-    this.router.navigate(["start-match"]);
+  login(nickname: string): Observable<Player> {
+    const newPlayer: NewPlayer = { nickname };
+    console.log(newPlayer);
+    return this.http
+      .post<Player>("http://localhost:5000/v1/player", newPlayer)
+      .pipe(
+        tap(player => {
+          localStorage.setItem(
+            AuthService.PLAYER_FIELD,
+            JSON.stringify(player)
+          );
+          this.router.navigate(["start-match"]);
+          return player;
+        })
+      );
   }
 
   logout() {
-    localStorage.removeItem(AuthService.LOGIN_FIELD);
+    localStorage.removeItem(AuthService.PLAYER_FIELD);
     this.router.navigate([""]);
   }
 
   isLogged(): boolean {
-    return localStorage.getItem(AuthService.LOGIN_FIELD) !== null;
+    return localStorage.getItem(AuthService.PLAYER_FIELD) !== null;
+  }
+
+  getPlayer(): Player {
+    return JSON.parse(localStorage.getItem(AuthService.PLAYER_FIELD));
   }
 }
